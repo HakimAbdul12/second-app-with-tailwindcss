3e5b7648a0b1ec2618dac638522fa272e901faf3
HAKIMABEN12 <124052578+HAKIMABEN12@users.noreply.github.com>
2024-04-11 17:13:51 +0000

    form intiate

--- src/app/pages/housing/details/details.component.ts
+++ src/app/pages/housing/details/details.component.ts
@@ -3,20 +3,48 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../../../services/housing/housing.service';
 import { HousingLocation } from '../../../interface/housinglocation';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule,
+    ReactiveFormsModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
 export class DetailsComponent {
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  });
+
+
+
+
+
+
+
+
+
+
+
+
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
+
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    );
   }
 }
--- src/app/services/housing/housing.service.ts
+++ src/app/services/housing/housing.service.ts
@@ -114,7 +114,13 @@ export class HousingService {
     return this.housingLocationList;
   }
 
+
   getHousingLocationById(id: number): HousingLocation | undefined {
     return this.housingLocationList.find(housingLocation => housingLocation.id === id);
   }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(`Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`);
+  }
+
 }
